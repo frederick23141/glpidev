0c9b75492ca81c68297b66b48e1937a64e149f16
FeDev <federicotoro23141@gmail.com>
2025-02-20 13:59:16 -0500

    fig
    a

--- config/config_db.php
+++ config/config_db.php
@@ -1,14 +1,29 @@
+//
 <?php
-$CFG_GLPI['url'] = 'http://10.10.10.244/glpi/';
+//$CFG_GLPI['url'] = 'http://10.10.10.244/glpi/';
+//class DB extends DBmysql {
+//   public $dbhost = 'localhost';
+//   public $dbuser = 'root';
+//   public $dbpassword = '';
+//   public $dbdefault = 'helpdesk';
+//   public $use_timezones = true;
+//   public $use_utf8mb4 = true;
+//   public $allow_myisam = false;
+//   public $allow_datetime = false;
+//   public $allow_signed_keys = false;
+//}
+$CFG_GLPI['url'] = 'https://mesa.azurewebsites.net/';
+
 class DB extends DBmysql {
-   public $dbhost = 'localhost';
-   public $dbuser = 'root';
-   public $dbpassword = '';
-   public $dbdefault = 'helpdesk';
+   public $dbhost = 'adminhelpdesk.mysql.database.azure.com'; // Nombre del servidor en Azure
+   public $dbuser = 'Adminservice'; // Usuario de la base de datos en Azure
+   public $dbpassword = 'Appservices.>'; // Contraseña de la base de datos en Azure
+   public $dbdefault = 'helpdesk'; // Nombre de la base de datos
    public $use_timezones = true;
    public $use_utf8mb4 = true;
    public $allow_myisam = false;
    public $allow_datetime = false;
    public $allow_signed_keys = false;
 }
+
 
